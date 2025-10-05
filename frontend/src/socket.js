184877d0fc33b3ec2e52522e4f534063f552ed40
SaiShivaniPakala <83344798+SaiShivaniPakala@users.noreply.github.com>
2025-10-05 14:28:38 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
 // The URL of your backend server
-const SOCKET_URL = 'http://localhost:4000';
+const SOCKET_URL = 'https://polling-system-backend-aim7.onrender.com';
 export const socket = io(SOCKET_URL);
